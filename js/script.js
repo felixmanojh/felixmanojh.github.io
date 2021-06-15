86efd9ffc884e6eed16d037cdc7eb17c35e0e636
felixmanojh <felixmanojh@gmail.com>
2021-06-15 23:58:25 +0530

    new
    v1.1

--- js/script.js
+++ js/script.js
@@ -44,7 +44,8 @@ monogatari.assets ('gallery', {
 
 // Define the music used in the game.
 monogatari.assets ('music', {
-	'm1': 'Ana.mp3'
+	'm1': 'Ana.mp3',
+	'm2': 'sc2.mp3'
 
 });
 
@@ -70,7 +71,8 @@ monogatari.assets ('images', {
 
 // Define the backgrounds for each scene.
 monogatari.assets ('scenes', {
-	'im0':'sc1.jpg'  ,
+	'im0':'sc1.jpg' ,
+	'im1':'sc2.jpg'
 
 });
 
@@ -89,6 +91,19 @@ monogatari.characters ({
 			"Normal": "Normal.png",
 			"Happy": "Party.png"
 		}
+	},
+	'b':{
+		"Name": "Lewis",
+		"Color": "#00bfff",
+	},
+	'l':{
+		"Name": "Lucy",
+		"Color": "#00bfff",
+		"Directory": "Lucy",
+		"Images":{
+			"Lucy": "Lucy.png"
+		}
+
 	}
 });
 
@@ -117,6 +132,58 @@ monogatari.script ({
 		'a Hey Gunner, I think they are calling out ur name. All the best..',
 		'g Thanks. Ill see you soon.',
 		'stop music m1',
+
+		'show background im1',
+		'play music m2',
+		'b Hello Gunner. How are you doing today. My name is Lewis and I head the trading team at HSBC',
+		'g I am doing good sir. How about you ?',
+		'b Good Good. So shall we get started ?',
+		'g Yes sir. I am ready',
+		'b So tell me about yourself',
+		'g Myself David Gunner . Born and brought up at Washington DC . ',
+		'g Completed my schooling in New York and moved to Washington for completing my MBA. ',
+		'g I have been coding from the age of 9 and I am deeply interested in IB trading, CDOs . ',
+		'g In my freetime I watch anime and read financial magazines & research on cryptography',
+		'b You watch anime ? at this age?',
+		'g Sir, I dont think watching anime has any age requirement. I find their story plots very engaging',
+		'b Oh i see. Cool lets check your financial knowledge',
+		'b what are the different types of money?',
+		'g There are 5 different types of money: Fiat, commodity, representative, fiduciary, and commercial bank money',
+		'b Good. Why do we need investment banks',
+		'g The primary role of an investment bank is to advise government and businesses on how to meet their financial needs and help procure their finances',
+		'g Without us, the world would not have progressed at this rate',
+		'b Impressive. Last question for you',
+		'b Why should we select you?',
+		'g I like to play with numbers and from the very starting, I am very good at it. ',
+		'g I also have very good analytical and logical abilities and I have always liked to solve such mental abilities questions.',
+		'g I am also the batch topper ',
+		'b Nice. Do you have any quesntions for me',
+		'g I would like to know about the culture of the team that you worked with',
+		'b We have a very open culture. you can feel at ease to make your point without being judges. and yes, high performers get promoted fast <wink>',
+		'g good to know sir. thank you',
+		'b Sayonara! Gunner',
+		'g Arigato gozaimasu',
+		
+
+	
+		'show background im0',
+		'show character l Lucy at left with fadeIn',
+		'l We would like the following candidates to stay back - Bella, Daphne, Gunner, Ana',
+		'stop music m2',
+		'show character a Normal at right with fadeIn',
+		'play music m1',
+		'a Congrats Gunner',
+		'g Congrats Ana. We cracked the interview successfully',
+		'a Yes we made it. Never give up - Thats my ninja way as well 😊',
+		'l We invite the selcted candidates to join us for drinks and dinner at hard Rock cafe tonight at 8pm . Please come in casuals ',
+		'l See you all soon',
+		'g Do you want to go together tonight ?',
+		'a Yes we all 4 should go together tonight',
+		'g Yeah, you are right',
+		'stop music m1',
+		'g <Damn! four is a crowd >',
+
+
 
 
 
